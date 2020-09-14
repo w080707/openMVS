7811aa75ae2cf0f7f1e1ec36e40681aa97e0e4f1
Ethan <w080707@126.com>
2020-09-14 14:49:39 +0800

    [ExportData] Add a new module;

---
+++ apps/ExportData/ExportData.cpp
@@ -0,0 +1,274 @@
+/*
+ * ReconstructMesh.cpp
+ *
+ * Copyright (c) 2014-2015 SEACAVE
+ *
+ * Author(s):
+ *
+ *      cDc <cdc.seacave@gmail.com>
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ */
+
+#include "../../libs/MVS/Common.h"
+#include "../../libs/MVS/Scene.h"
+#include <boost/program_options.hpp>
+
+using namespace MVS;
+
+
+// D E F I N E S ///////////////////////////////////////////////////
+
+#define APPNAME _T("ReconstructMesh")
+
+// uncomment to enable multi-threading based on OpenMP
+#ifdef _USE_OPENMP
+#define RECMESH_USE_OPENMP
+#endif
+
+
+// S T R U C T S ///////////////////////////////////////////////////
+
+namespace OPT {
+String strInputFileName;
+String strOutputFileName;
+String strMeshFileName;
+bool bMeshExport;
+float fDistInsert;
+bool bUseConstantWeight;
+bool bUseFreeSpaceSupport;
+float fThicknessFactor;
+float fQualityFactor;
+float fDecimateMesh;
+float fRemoveSpurious;
+bool bRemoveSpikes;
+unsigned nCloseHoles;
+unsigned nSmoothMesh;
+unsigned nArchiveType;
+int nProcessPriority;
+unsigned nMaxThreads;
+String strExportType;
+String strConfigFileName;
+boost::program_options::variables_map vm;
+} // namespace OPT
+
+// initialize and parse the command line parameters
+bool Initialize(size_t argc, LPCTSTR* argv)
+{
+	// initialize log and console
+	OPEN_LOG();
+	OPEN_LOGCONSOLE();
+
+	// group of options allowed only on command line
+	boost::program_options::options_description generic("Generic options");
+	generic.add_options()
+		("help,h", "produce this help message")
+		("working-folder,w", boost::program_options::value<std::string>(&WORKING_FOLDER), "working directory (default current directory)")
+		("config-file,c", boost::program_options::value<std::string>(&OPT::strConfigFileName)->default_value(APPNAME _T(".cfg")), "file name containing program options")
+		("export-type", boost::program_options::value<std::string>(&OPT::strExportType)->default_value(_T("ply")), "file type used to export the 3D scene (ply or obj)")
+		("archive-type", boost::program_options::value<unsigned>(&OPT::nArchiveType)->default_value(2), "project archive type: 0-text, 1-binary, 2-compressed binary")
+		("process-priority", boost::program_options::value<int>(&OPT::nProcessPriority)->default_value(-1), "process priority (below normal by default)")
+		("max-threads", boost::program_options::value<unsigned>(&OPT::nMaxThreads)->default_value(0), "maximum number of threads (0 for using all available cores)")
+		#if TD_VERBOSE != TD_VERBOSE_OFF
+		("verbosity,v", boost::program_options::value<int>(&g_nVerbosityLevel)->default_value(
+			#if TD_VERBOSE == TD_VERBOSE_DEBUG
+			3
+			#else
+			2
+			#endif
+			), "verbosity level")
+		#endif
+		;
+
+	// group of options allowed both on command line and in config file
+	boost::program_options::options_description config_main("Reconstruct options");
+	config_main.add_options()
+		("input-file,i", boost::program_options::value<std::string>(&OPT::strInputFileName), "input filename containing camera poses and image list")
+		("output-file,o", boost::program_options::value<std::string>(&OPT::strOutputFileName), "output filename for storing the mesh")
+		("min-point-distance,d", boost::program_options::value<float>(&OPT::fDistInsert)->default_value(2.5f), "minimum distance in pixels between the projection of two 3D points to consider them different while triangulating (0 - disabled)")
+		("constant-weight", boost::program_options::value<bool>(&OPT::bUseConstantWeight)->default_value(true), "considers all view weights 1 instead of the available weight")
+		("free-space-support,f", boost::program_options::value<bool>(&OPT::bUseFreeSpaceSupport)->default_value(false), "exploits the free-space support in order to reconstruct weakly-represented surfaces")
+		("thickness-factor", boost::program_options::value<float>(&OPT::fThicknessFactor)->default_value(1.f), "multiplier adjusting the minimum thickness considered during visibility weighting")
+		("quality-factor", boost::program_options::value<float>(&OPT::fQualityFactor)->default_value(1.f), "multiplier adjusting the quality weight considered during graph-cut")
+		;
+	boost::program_options::options_description config_clean("Clean options");
+	config_clean.add_options()
+		("decimate", boost::program_options::value<float>(&OPT::fDecimateMesh)->default_value(1.f), "decimation factor in range (0..1] to be applied to the reconstructed surface (1 - disabled)")
+		("remove-spurious", boost::program_options::value<float>(&OPT::fRemoveSpurious)->default_value(20.f), "spurious factor for removing faces with too long edges or isolated components (0 - disabled)")
+		("remove-spikes", boost::program_options::value<bool>(&OPT::bRemoveSpikes)->default_value(true), "flag controlling the removal of spike faces")
+		("close-holes", boost::program_options::value<unsigned>(&OPT::nCloseHoles)->default_value(30), "try to close small holes in the reconstructed surface (0 - disabled)")
+		("smooth", boost::program_options::value<unsigned>(&OPT::nSmoothMesh)->default_value(2), "number of iterations to smooth the reconstructed surface (0 - disabled)")
+		;
+
+	// hidden options, allowed both on command line and
+	// in config file, but will not be shown to the user
+	boost::program_options::options_description hidden("Hidden options");
+	hidden.add_options()
+		("mesh-file", boost::program_options::value<std::string>(&OPT::strMeshFileName), "mesh file name to clean (skips the reconstruction step)")
+		("mesh-export", boost::program_options::value<bool>(&OPT::bMeshExport)->default_value(false), "just export the mesh contained in loaded project")
+		;
+
+	boost::program_options::options_description cmdline_options;
+	cmdline_options.add(generic).add(config_main).add(config_clean).add(hidden);
+
+	boost::program_options::options_description config_file_options;
+	config_file_options.add(config_main).add(config_clean).add(hidden);
+
+	boost::program_options::positional_options_description p;
+	p.add("input-file", -1);
+
+	try {
+		// parse command line options
+		boost::program_options::store(boost::program_options::command_line_parser((int)argc, argv).options(cmdline_options).positional(p).run(), OPT::vm);
+		boost::program_options::notify(OPT::vm);
+		INIT_WORKING_FOLDER;
+		// parse configuration file
+		std::ifstream ifs(MAKE_PATH_SAFE(OPT::strConfigFileName));
+		if (ifs) {
+			boost::program_options::store(parse_config_file(ifs, config_file_options), OPT::vm);
+			boost::program_options::notify(OPT::vm);
+		}
+	}
+	catch (const std::exception& e) {
+		LOG(e.what());
+		return false;
+	}
+
+	// initialize the log file
+	OPEN_LOGFILE(MAKE_PATH(APPNAME _T("-")+Util::getUniqueName(0)+_T(".log")).c_str());
+
+	// print application details: version and command line
+	Util::LogBuild();
+	LOG(_T("Command line:%s"), Util::CommandLineToString(argc, argv).c_str());
+
+	// validate input
+	Util::ensureValidPath(OPT::strInputFileName);
+	Util::ensureUnifySlash(OPT::strInputFileName);
+	if (OPT::vm.count("help") || OPT::strInputFileName.IsEmpty()) {
+		boost::program_options::options_description visible("Available options");
+		visible.add(generic).add(config_main).add(config_clean);
+		GET_LOG() << visible;
+	}
+	if (OPT::strInputFileName.IsEmpty())
+		return false;
+	OPT::strExportType = OPT::strExportType.ToLower() == _T("obj") ? _T(".obj") : _T(".ply");
+
+	// initialize optional options
+	Util::ensureValidPath(OPT::strOutputFileName);
+	Util::ensureUnifySlash(OPT::strOutputFileName);
+	if (OPT::strOutputFileName.IsEmpty())
+		OPT::strOutputFileName = Util::getFileFullName(OPT::strInputFileName) + _T("_mesh.mvs");
+
+	// initialize global options
+	Process::setCurrentProcessPriority((Process::Priority)OPT::nProcessPriority);
+	#ifdef _USE_OPENMP
+	if (OPT::nMaxThreads != 0)
+		omp_set_num_threads(OPT::nMaxThreads);
+	#endif
+
+	#ifdef _USE_BREAKPAD
+	// start memory dumper
+	MiniDumper::Create(APPNAME, WORKING_FOLDER);
+	#endif
+
+	Util::Init();
+	return true;
+}
+
+// finalize application instance
+void Finalize()
+{
+	#if TD_VERBOSE != TD_VERBOSE_OFF
+	// print memory statistics
+	Util::LogMemoryInfo();
+	#endif
+
+	CLOSE_LOGFILE();
+	CLOSE_LOGCONSOLE();
+	CLOSE_LOG();
+}
+
+int main(int argc, LPCTSTR* argv)
+{
+	#ifdef _DEBUGINFO
+	// set _crtBreakAlloc index to stop in <dbgheap.c> at allocation
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);// | _CRTDBG_CHECK_ALWAYS_DF);
+	#endif
+
+	if (!Initialize(argc, argv))
+		return EXIT_FAILURE;
+
+	Scene scene(OPT::nMaxThreads);
+	// load project
+	if (!scene.Load(MAKE_PATH_SAFE(OPT::strInputFileName)))
+		return EXIT_FAILURE;
+	
+	// Binary write
+	std::string filename = "/home/hp/Desktop/cameras_krc.bin";
+	FILE* fwp = fopen(filename.c_str(), "wb");
+	
+	// Export cameras
+	std::ofstream sfwrite("/home/hp/Desktop/camera.txt", std::ios::out);
+	std::ofstream sfwrite_krc("/home/hp/Desktop/KRC.txt", std::ios::out);
+
+	int num_images = scene.images.size();
+	std::cout << "Platform  size is \n" << scene.platforms.size() << "\n";
+	std::cout << "Platform camera size is \n" << scene.platforms[0].cameras.size() << "\n";
+	std::cout << "Platform camera is \n" << scene.platforms[0].cameras[0].K << "\n";
+	
+	fwrite(&(num_images), sizeof(int), 1, fwp);
+	for (int i = 0; i < num_images; i++){
+		std::cout << scene.images[i].name << "\n";
+		std::cout << scene.images[i].width << "\n";
+		std::cout << scene.images[i].height << "\n";
+		
+		// auto name = scene.images[i].name.c_str();
+		// int name_len = sizeof(name);
+		// fwrite(&(name_len), sizeof(int), 1, fwp);
+		// fwrite(name, sizeof(name[0]), name_len, fwp);
+
+		std::vector<char> name(scene.images[i].name.begin(), scene.images[i].name.end());
+		int name_len = name.size();
+		fwrite(&(name_len), sizeof(int), 1, fwp);
+		fwrite(&name[0], sizeof(name[0]), name_len, fwp);
+
+		fwrite(&(scene.images[i].camera.K), sizeof(scene.images[i].camera.K), 1, fwp);
+		fwrite(&(scene.images[i].camera.R), sizeof(scene.images[i].camera.R), 1, fwp);
+		fwrite(&(scene.images[i].camera.C), sizeof(scene.images[i].camera.C), 1, fwp);
+		
+		sfwrite_krc << "ImagePath\n" << scene.images[i].name << "\n";
+		sfwrite_krc << "K \n" << scene.images[i].camera.K << "\n";
+		sfwrite_krc << "R \n" << scene.images[i].camera.R << "\n";
+		sfwrite_krc << "C \n" << scene.images[i].camera.C << "\n\n";
+
+		std::cout << i << "Image scale is " << scene.images[i].scale << "\n\n";
+		sfwrite << "ImagePath\n" << scene.images[i].name << "\n";
+		sfwrite << "CameraPose\n" << scene.images[i].camera.P << "\n\n";
+	}
+	sfwrite.close();
+	sfwrite_krc.close();
+	fclose(fwp);
+
+	Finalize();
+	return EXIT_SUCCESS;
+}
+/*----------------------------------------------------------------*/
